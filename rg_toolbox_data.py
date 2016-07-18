ea0ac001d86c5bd20f6f7c41d8415e9ddc73d450
Deckard <rghigliaus@yahoo.com>
2016-07-18 16:04:59 -0400

    Improved data type recognition

--- rg_toolbox_data.py
+++ rg_toolbox_data.py
@@ -15,25 +15,109 @@ This is a start of a toolbox for plots
 #from rg_toolbox_data import preproc_data
 #data_num, data_bol, data_bin, data_mlnd = preproc_data(data)
 
+from __future__ import division
 
 
 # Import libraries
 import numpy as np
 import pandas as pd
-from pandas import DataFrame, Series
+
+# -RG 7/15/2016
+#def data_types(data, n_thr=5):
+#    # You need to determine both type and support
+#    d_typ = DataFrame(data.dtypes, columns = ['type']) # data.dtypes a Series with Index = column name
+#    d_typ['support'] = 'continuous'
+#    for col, x in data.iteritems():
+#        if x.dtypes!=np.object:
+#            z_tmp = x.value_counts()  # Series
+#            if len(z_tmp)<= n_thr: d_typ.ix[col, 'support'] = 'categorical'
+#        else:
+#            z_tmp = x.unique()        # array
+#            if len(z_tmp)<= n_thr: d_typ.ix[col, 'support'] = 'categorical'
+#    return d_typ
+
+#from datetime import datetime
+import dateutil.parser
 
 def data_types(data, n_thr=5):
     # You need to determine both type and support
-    d_typ = DataFrame(data.dtypes, columns = ['type']) # data.dtypes a Series with Index = column name
-    d_typ['support'] = 'continuous'
+    # n_thr determines how many different values a column may assume before it is considered as continuous support
+    d_typ = pd.DataFrame(data.dtypes, columns = ['type']) # data.dtypes a Series with Index = column name
+    d_typ['subtype'] = '' # add a column for sub-type
+    d_typ['support'] = '' # add a column for support
     for col, x in data.iteritems():
+        print "Processing column '{}'".format(col)
         if x.dtypes!=np.object:
             z_tmp = x.value_counts()  # Series
-            if len(z_tmp)<= n_thr: d_typ.ix[col, 'support'] = 'categorical'
+            if len(z_tmp)<= n_thr:
+                d_typ.ix[col, 'support'] = 'discrete'
+            else:
+                d_typ.ix[col, 'support'] = 'continuous'
         else:
             z_tmp = x.unique()        # array
-            if len(z_tmp)<= n_thr: d_typ.ix[col, 'support'] = 'categorical'
+            if len(z_tmp)<=n_thr:
+                d_typ.ix[col, 'support'] = 'discrete'
+            else:
+                d_typ.ix[col, 'support'] = 'continuous'
+            # Try to determine the type further
+            s_tmp = x.dropna()
+            if len(s_tmp)>0:
+                if type(s_tmp.iloc[0])==type('string'):
+                    # Try to see if it is a numerical variable
+                    try:
+                        int(s_tmp.iloc[0])
+                        d_typ.ix[col, 'type'] = 'num'
+                        d_typ.ix[col, 'subtype'] = 'int'
+                    except:
+                        try:
+                            float(s_tmp.iloc[0])
+                            d_typ.ix[col, 'type'] = 'num'
+                            d_typ.ix[col, 'subtype'] = 'float'
+                        except:
+                            # Try to see if it is a time variable
+                            try:
+                                dateutil.parser.parse(s_tmp.iloc[0])
+                                d_typ.ix[col, 'type'] = 'time'
+                                d_typ.ix[col, 'subtype'] = 'time'
+                            except ValueError:
+                                d_typ.ix[col, 'type'] = 'string'
+                                d_typ.ix[col, 'subtype'] = 'string'
+                            except:
+                                print 'Column {}: unrecognized type'.format(col)
+#                                print d_typ.ix[col, 'type'], d_typ.ix[col, 'subtype']
+                                d_typ.ix[col, 'type'] = 'string'
+                                d_typ.ix[col, 'subtype'] = 'string'
+#                                print d_typ.ix[col, 'type'], d_typ.ix[col, 'subtype']
+#                elif type(s_tmp.iloc[0])==type(1):
+#                    d_typ.ix[col, 'type'] = 'num'
+#                    d_typ.ix[col, 'subtype'] = 'int'
+                
     return d_typ
+
+
+from datetime import datetime
+
+def totimestamp(dt, epoch=datetime(1970,1,1)):
+    # Example
+#    now = datetime.utcnow()
+#    print now
+#    print totimestamp(now)
+
+    td = dt - epoch
+    # return td.total_seconds()
+    return (td.microseconds + (td.seconds + td.days * 86400) * 10**6) / 10**6 
+
+def str2timestamp(dt_str, epoch=datetime(1970,1,1)):
+#    if dt_str!='nan' | np.isnan(dt_str):
+    if dt_str!='nan':
+        td = epoch
+    else:
+        dt = dateutil.parser.parse(dt_str)
+        td = dt - epoch
+        
+    # return td.total_seconds()
+    return (td.microsecond + (td.second + td.day * 86400) * 10**6) / 10**6 
+
 
 def create_buckets(x, nB=5):
     # Create buckets
@@ -55,7 +139,7 @@ def preproc_data(data, nL_max = 4):
     # First convert into numerical values
     # Then convert those into boolean (use dummy functionality)
     # Then convert to binary representation
-    data_num = DataFrame(index=data.index) # a numerical version of the data
+    data_num = pd.DataFrame(index=data.index) # a numerical version of the data
     
     for col, col_data in data.iteritems():  # text and Series
         print "Processing column: ", col, ", data type = ", col_data.dtype
@@ -80,7 +164,7 @@ def preproc_data(data, nL_max = 4):
     return data_num
 
 def cat2num(data):
-    data_num = DataFrame(index=data.index) # a numerical version of the data
+    data_num = pd.DataFrame(index=data.index) # a numerical version of the data
     
     for col, col_data in data.iteritems():  # text and Series
         print "Processing column: ", col, ", data type = ", col_data.dtype
@@ -119,7 +203,7 @@ def cat_series2num(x):
 def preproc_data_mlnd(data):
 
     # MLND
-    data_mlnd= DataFrame(index=data.index) # a MLND version of the data
+    data_mlnd= pd.DataFrame(index=data.index) # a MLND version of the data
     
     for col, col_data in data.iteritems():  # text and Series
         print "Processing column: ", col, ", data type = ", col_data.dtype
@@ -140,13 +224,13 @@ def preproc_data_mlnd(data):
 
 def df_num2bol(data):
     # Converts a numerical DataFrame into Boolean
-    data_bol = DataFrame(index=data.index) # a boolean version of the data
+    data_bol = pd.DataFrame(index=data.index) # a boolean version of the data
     return data_bol
 
 
 def df_num2bin(data):
     # Converts a numerical DataFrame into binary
-    data_bin = DataFrame(index=data.index) # a binary version of the data
+    data_bin = pd.DataFrame(index=data.index) # a binary version of the data
     return data_bin
 
 
@@ -165,10 +249,10 @@ def cut_to_df(data, col, b):
 
 # Old version
 def preproc_data_20160504(data):
-    data_num = DataFrame(index=data.index)
-    data_bol = DataFrame(index=data.index)
-    data_bin = DataFrame(index=data.index)
-    data_mlnd= DataFrame(index=data.index)
+    data_num = pd.DataFrame(index=data.index)
+    data_bol = pd.DataFrame(index=data.index)
+    data_bin = pd.DataFrame(index=data.index)
+    data_mlnd= pd.DataFrame(index=data.index)
     
     for col, col_data in data.iteritems():  # text and Series
         print "Processing column: ", col, ", data type = ", col_data.dtype
